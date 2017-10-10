e3ae51831669037593961fd45cbd4fb8abe665f3
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2017-10-10 11:26:47 +0700

    init commit base template

---
+++ nuxt.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  build: {
+    publicPath: '/nuxt-blog',
+    vendor: ['vue-i18n']
+  }
+  loading: { color: 'cyan' },
+  router: {
+    middleware: 'i18n'
+  },
+  plugins: ['~/plugins/i18n.js',],
+  generate: {
+    routes: ['/', '/about', '/fr', '/fr/about']
+  }
+}
